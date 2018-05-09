3865becdf001dc705eb6009b0a8d0bb1a2a1688a
Tate Peñaranda <tate@xenir.com.ar>
2018-05-09 02:05:54 -0300

    Fix for config key naming.

--- src/config/bcoin.php
+++ src/config/bcoin.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'bcoin_api_key' => env('BCOIN_API_KEY', false),
-    'bcoin_server_ip' => env('BCOIN_SERVER_IP', '127.0.0.1'),
-    'bcoin_server_port' => env('BCOIN_SERVER_PORT', 18333),
-    'bcoin_service_key' => env('BCOIN_SERVICE_KEY', false),
+    'api_key' => env('BCOIN_API_KEY', false),
+    'server_ip' => env('BCOIN_SERVER_IP', '127.0.0.1'),
+    'server_port' => env('BCOIN_SERVER_PORT', 18333),
+    'service_key' => env('BCOIN_SERVICE_KEY', false),
 ];
